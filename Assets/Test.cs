715cecb1417d6affecda4851594d48647992bfff
ayato-tasaki <petaniini@gmail.com>
2020-03-28 22:49:50 +0900

    ScriptTest配列宣言を出力
    コンソールビューへ任意の数字5個の配列を従順と逆順での表示ができました。

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    void Start () { 
+    
+        {
+            int[] array = new int[5];
+
+            array[0] = 28;
+            array[1] = 56;
+            array[2] = 14;
+            array[3] = 7;
+            array[4] = 35;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Debug.Log(array[i]);
+            }
+             for (int i = array.Length -1; i >= 0; i--)
+            {
+                Debug.Log(array[i]);
+            }
+        }
+        
+    }
+}
+	
